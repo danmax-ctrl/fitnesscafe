5280ad34a176da9f97310c8dbfb225f3e6ae9f17
danmax-ctrl <mushthaqueilan@gmail.com>
2025-12-24 10:34:23 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/fitnesscafe/", // 👈 MUST match repo name
+  base: "/danmax-ctrl/fitnesscafe/", // 👈 MUST match repo name
   plugins: [react()],
   resolve: {
     alias: {
